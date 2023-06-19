f762fb0e142c7799859882973c1b27d879121c3d
SquareZer0 <61067252+limon768@users.noreply.github.com>
2023-06-18 22:07:58 -0400

    More Updates

--- myscript.sh
+++ myscript.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-echo "My personal script to setup a new VM"
-
+echo "\n============================================================\n"
+echo "My personal script to setup a new VM\n"
 echo "Installing common tools"
-sleep 5
+echo "\n\n============================================================\n\n"
+
 
 sudo apt update && sudo apt dist-upgrade -y
 
-sudo apt install wget curl zsh tmux dirsearch vim remmina terminator
+sudo apt install wget curl zsh tmux dirsearch vim remmina terminator openjdk-17-jdk -y
 
+echo "\n============================================================\n"
 echo "Installing VsCode"
+echo "\n\n============================================================\n\n"
 sudo apt -y install code-oss
 
+echo "\n============================================================\n"
 echo "Installing Sublime"
+echo "\n\n============================================================\n\n"
 sleep 5
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
@@ -19,7 +24,9 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get install sublime-text
 
+echo "\n============================================================\n"
 echo "Installing Brave"
+echo "\n\n============================================================\n\n"
 sleep 5
 sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -28,7 +35,9 @@ sudo apt update
 sudo apt install brave-browser -y
 
 #optional
+echo "\n============================================================\n"
 echo "Moving VPN files"
+echo "\n\n============================================================\n\n"
 mv vpn ~/vpn
 mv .tmux.conf ~/.tmux.conf
 mv config ~/.config/terminator/config
@@ -42,14 +51,18 @@ if [[ $choice == "y" ]]; then
 	python3 -m pipx install impacket
 fi
 
-echo "Plugins for ZSH"
-#autosuggestion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+echo "\n============================================================\n"
 echo "Installing Oh-My-ZSH"
+echo "\n\n============================================================\n\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
 
+## Might have to restart the script
+echo "\n============================================================\n"
+echo "Plugins for ZSH"
+echo "\n\n============================================================\n\n"
+#autosuggestion
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
 
